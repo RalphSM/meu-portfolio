bfef08eb11d530bae4b2b32fa5ada3c7514f38fd
RalphSM <ralphsmateus@live.com>
2025-01-27 16:30:46 -0300

    Ajuste botão hamburguer mobile 2.0

--- src/Pages/Home/sections/NavBar/NavBar.tsx
+++ src/Pages/Home/sections/NavBar/NavBar.tsx
@@ -1,4 +1,4 @@
-import { AppBar, Drawer, IconButton, List, ListItem, ListItemText, MenuItem, styled, Toolbar } from "@mui/material"
+import { AppBar, Divider, Drawer, IconButton, List, ListItem, ListItemText, MenuItem, styled, Toolbar } from "@mui/material"
 import MenuIcon from '@mui/icons-material/Menu';
 import { useState } from "react";
 
@@ -15,20 +15,24 @@ const NavBar = () => {
         setOpenDrawer(open);
       }
 
+      const handleItemClick = () => {
+        toggleDrawer(false);
+      };
+
     return (
       <>
         <AppBar position="fixed">
             <StyledToobar>
-                <MenuItem component="a" href="#home">Home</MenuItem>
-                <MenuItem component="a" href="#sobre">Sobre</MenuItem>
-                <MenuItem component="a" href="#skills">Habilidades</MenuItem>
-                <MenuItem component="a" href="#projetos">Projetos</MenuItem>
+                <MenuItem component="a" href="#home" sx={{ display: { xs: 'none', md: 'block' } }}>Home</MenuItem>
+                <MenuItem component="a" href="#sobre" sx={{ display: { xs: 'none', md: 'block' } }}>Sobre</MenuItem>
+                <MenuItem component="a" href="#skills" sx={{ display: { xs: 'none', md: 'block' } }}>Habilidades</MenuItem>
+                <MenuItem component="a" href="#projetos" sx={{ display: { xs: 'none', md: 'block' } }}>Projetos</MenuItem>
 
                 <IconButton
                 color="inherit"
                 aria-label="menu"
                 edge= "end"
-                onClick={ ()  => toggleDrawer(false) } 
+                onClick={ ()  => toggleDrawer(true) } 
                 sx={{ display:{xs:"block", md:"none"} }}
                 >
                   <MenuIcon/>
@@ -39,18 +43,39 @@ const NavBar = () => {
         anchor="right"
         open={openDrawer}
         onClose={() => toggleDrawer(false)}
+        sx={{
+         '& .MuiDrawer-papper': {
+            width: 250,
+            backgroundColor: "#333",
+            color: "#fff",
+            borderLeft: "2px solid #444"
+         }
+         
+
+        }}
       >
         <List>
-          <ListItem component="a" href="#home">
-            <ListItemText primary="Home" />
-          </ListItem>
-          <ListItem component="a" href="#sobre">
-            <ListItemText primary="Sobre" />
-          </ListItem>
-          <ListItem component="a" href="#projetos">
-            <ListItemText primary="Projetos" />
-          </ListItem>
+        <ListItem component="a" href="#home" sx={{ padding: '15px 20px', color: '#000', '&:hover': { backgroundColor: '#444' } }} onClick={handleItemClick}>
+          <ListItemText primary="Home" />
+        </ListItem>
+        <Divider sx={{ backgroundColor: '#444' }} />
+
+        <ListItem component="a" href="#sobre" sx={{ padding: '15px 20px', color: '#000', '&:hover': { backgroundColor: '#444' } }} onClick={handleItemClick}>
+          <ListItemText primary="Sobre" />
+        </ListItem>
+        <Divider sx={{ backgroundColor: '#444' }} />
+
+        <ListItem component="a" href="#skills" sx={{ padding: '15px 20px', color: '#000', '&:hover': { backgroundColor: '#444' } }} onClick={handleItemClick}>
+          <ListItemText primary="Habilidades" />
+        </ListItem>
+        <Divider sx={{ backgroundColor: '#444' }} />
+
+        <ListItem component="a" href="#projetos" sx={{ padding: '15px 20px', color: '#000', '&:hover': { backgroundColor: '#444' } }} onClick={handleItemClick}>
+          <ListItemText primary="Projetos" />
+        </ListItem>
+        <Divider sx={{ backgroundColor: '#444' }} />
         </List>
+
       </Drawer>
       </>
     )
